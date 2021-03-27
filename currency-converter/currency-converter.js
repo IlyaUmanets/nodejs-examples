3953dfc97bf85c3a0339d4e457d69edcd196b726
IlyaUmanets <ilya.umanets.web@gmail.com>
2021-03-27 18:06:19 +0200

    Install expresjs and nodemon

--- currency-converter/currency-converter.js
+++ currency-converter/currency-converter.js
@@ -1,3 +1,6 @@
+const express = require('express')
+const app = express()
+const port = 3000
 const axios = require('axios');
 
 const getExchangeRate = async(fromCurrency, toCurrency) => {
@@ -31,9 +34,19 @@ const convertCurrency = async(fromCurrency, toCurrency, amount) => {
           You can spend these in the following countries: ${countries}`;
 };
 
-convertCurrency('USD', 'HRK', 20)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/currency', (req, res) => {
+  convertCurrency('USD', 'HRK', 20)
   .then((message) => {
-    console.log(message);
+    res.send(message)
   }).catch((error) => {
-    console.log(error.message);
+    res.send(error)
   });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
